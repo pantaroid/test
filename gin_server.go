0eed22cb55050ec3c2714f18e2ed6c59c4d07661
pantaroid <sanada@Darwin.local>
2017-02-17 22:42:49 +0900

    add udp sample

--- gin_server.go
+++ gin_server.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+  "github.com/braintree/manners"
   "gopkg.in/gin-gonic/gin.v1"
   "net/http"
+  "net"
   "fmt"
   "os"
+  "os/signal"
   "io"
   "io/ioutil"
+  "syscall"
+  "strings"
+  "strconv"
 )
 
 type UploadedFile struct {
@@ -14,18 +20,31 @@ type UploadedFile struct {
   Time string//time.Time
 }
 
-type Node struct {
-  Id int
+type ServiceServer struct {
+  Port int
+  Status int
   Name string
   Addr string
-  Status int
+  Node Node
 }
 
-const dateTimeLayout = "2006-01-02 15:04:05"
+type Node struct {
+  Status int
+  Name string
+  Addr string
+  ServiceServers []ServiceServer
+}
 
-var nodes = make(map[string]Node)
+type HubInfo struct {
+  Nodes []Node
+  Domains map[string][]ServiceServer
+}
 
-func index(c *gin.Context) {
+const (
+  dateTimeLayout = "2006-01-02 15:04:05"
+)
+
+func index(c *gin.Context, h *HubInfo) {
   files, _ := ioutil.ReadDir("./files")
   size := len(files)
   lists := make([]UploadedFile, size)
@@ -34,10 +53,11 @@ func index(c *gin.Context) {
   }
   c.HTML(http.StatusOK, "index.tmpl", gin.H {
     "files": lists,
+    "nodes": h.Nodes,
   })
 }
 
-func execute(c *gin.Context) {
+func execute(c *gin.Context, h *HubInfo) {
   var json map[string]interface{}
   err := c.BindJSON(&json)
   if err != nil {
@@ -53,7 +73,7 @@ func execute(c *gin.Context) {
     default:
   }
 
-  index(c)
+  index(c, h)
   //c.Redirect(http.StatusMovedPermanently, "/")
 }
 
@@ -100,25 +120,119 @@ func download(c *gin.Context) {
 
 func main() {
 
-  // initailize
-  router := gin.Default()
+  cInfo := make(chan *HubInfo)
+  fInfo := func(fn func(*HubInfo)) {
+    info :=  <- cInfo
+    fn(info)
+    cInfo <- info
+  }
 
-  // load templates
-  router.LoadHTMLGlob("./templates/*")
 
-  // resources
-  router.StaticFile("/script.js", "./resources/script.js")
+  {// ResourceMaster
+    go func() {
+      var info *HubInfo
+      info = &HubInfo{}
+      // debug
+      info.Nodes = append(info.Nodes, Node { Status: 0, Name: "TEST", Addr: "Sample" })
 
-  // root 
-  router.GET("/", index)
+      for {
+        select {
+          case cInfo <- info:
+            info = <- cInfo
+        }
+      }
+    }()
+  }
 
-  // loaders
-  router.POST("/upload", upload)
-  router.GET("/download/:file", download)
+  {// CommunicationServer
+    fmt.Println("UDP START!!")
+    addr, err := net.ResolveUDPAddr("udp", ":51702")
+    if err != nil {
+      fmt.Printf("Error: %s\n", err)
+      return
+    }
+    conn, err := net.ListenUDP("udp", addr)
+    if err != nil {
+      fmt.Printf("Error: %s\n", err)
+      return
+    }
+    defer conn.Close()
 
-  // execute
-  router.POST("/execute", execute)
+    go func() {
+      buf := make([]byte, 1024)
+      for {
+        rlen, remote, err := conn.ReadFromUDP(buf)
+        if err == nil {
+          message := string(buf[:rlen])
+          //rlen, err = conn.WriteToUDP([]byte(s), remote)
+          fmt.Printf("Receive [%v]: %v\n", remote, message)
 
-  router.Run(":8080")
+          // test
+          if strings.HasPrefix(message, "A") {
+            parts := strings.Split(message, ":")
+            for index, element := range parts {
+              fmt.Printf("  %d: [%s]\n", index, element)
+            }
+
+            fInfo(func(info *HubInfo) {
+              status, _ := strconv.Atoi(parts[1])
+              info.Nodes = append(info.Nodes, Node {
+                Status: status,
+                Name: parts[2],
+                Addr: parts[3],
+              })
+            })
+          }
+        }
+
+        //n, addr, err := ServerConn.ReadFromUDP(buf)
+        //fmt.Println("Received ",string(buf[0:n]), " from ",addr)
+        //if err != nil {
+        //  fmt.Println("Error: ",err)
+        //} 
+      }
+    }()
+  }
+
+  {// WebServer
+    // initailize
+    router := gin.Default()
+    // load templates
+    router.LoadHTMLGlob("./templates/*")
+    // resources
+    router.StaticFile("/script.js", "./resources/script.js")
+    // root
+    //router.GET("/", index)
+    router.GET("/", func(c *gin.Context) {
+      fInfo(func(info *HubInfo) {
+        fmt.Printf("HubInfo.Nodes [%v]\n", info.Nodes)
+        index(c, info)
+      })
+   })
+
+    // loaders
+    router.POST("/upload", upload)
+    router.GET("/download/:file", download)
+    // execute
+    router.POST("/execute", func(c *gin.Context) {
+      fInfo(func(info *HubInfo) {
+        execute(c, info)
+      })
+    })
+
+    manners.ListenAndServe(":51700",  router)
+    //router.Run(":51700")
+  }
+
+  signal_chan := make(chan os.Signal)
+  signal.Notify(signal_chan, syscall.SIGTERM)
+  go func() {
+      for {
+          s := <-signal_chan
+          if s == syscall.SIGTERM {
+              manners.Close()
+          }
+      }
+  }()
 }
 
